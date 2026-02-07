7109b72714e2f90622f53d6d9c1b5dbeea074516
Nala <nalanda.mahabarata@gmail.com>
2026-02-08 00:06:06 +0700

    Add files via upload

---
+++ stopwatch/script.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+
+  var seconds = 0;
+  var tens = 0;
+  var appendTens = document.getElementById("tens");
+  var appendSeconds = document.getElementById("seconds");
+  var buttonStart = document.getElementById("button-start");
+  var buttonStop = document.getElementById("button-stop");
+  var Interval ; 
+
+  buttonStart.onclick = function() {
+     clearInterval(Interval);
+     Interval = setInterval(startTimer, 10);
+  }
+
+  buttonStop.onclick = function() {
+     clearInterval(Interval);
+  }
+
+  function startTimer () {
+     tens = tens+1;
+
+     if (tens <= 9){
+       appendTens.innerHTML = "0" + tens;
+     }
+
+     if (tens > 9){
+       appendTens.innerHTML = tens; 
+     }
+
+     if (tens > 99){
+       seconds = seconds + 1;
+       appendSeconds.innerHTML = "0" + seconds;
+       tens = 0;
+       appendTens.innerHTML = "0" + 0;
+     }
+
+     if (seconds > 9){
+       appendSeconds.innerHTML = seconds;
+     }
+   }
+
+}
